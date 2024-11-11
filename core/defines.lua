14999a3cc79360df44f8807270d201c69319f8ee
Ritn <ritn@hotmail.fr>
2024-11-11 17:44:55 +0100

    remove module surface

--- core/defines.lua
+++ core/defines.lua
@@ -21,7 +21,7 @@ local defines = {
         ----
         force = mod_name .. ".modules.force",
         player = mod_name .. ".modules.player",
-        surface = mod_name .. ".modules.surface",
+        --surface = mod_name .. ".modules.surface",
         lobby = mod_name .. ".modules.lobby",
     },
 
--- core/modules.lua
+++ core/modules.lua
@@ -8,11 +8,10 @@ modules.events =                require(ritnlib.defines.base.modules.events)
 modules.commands =              require(ritnlib.defines.base.modules.commands)
 
 ------------------------------------------------------------------------------
-modules.force =                require(ritnlib.defines.base.modules.force)
+modules.force =                 require(ritnlib.defines.base.modules.force)
 ------------------------------------------------------------------------------
 -- Modules désactivable
 modules.player =                require(ritnlib.defines.base.modules.player)  
-modules.surface =               require(ritnlib.defines.base.modules.surface) 
 modules.lobby =                 require(ritnlib.defines.base.modules.lobby) 
 ------------------------------------------------------------------------------
 return modules
